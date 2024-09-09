14476d5fb5c58000f5a22d3869422e544f7f4469
Angel-Zarate <22121075@morelia.tecnm.mx>
2024-09-09 00:16:05 -0600

    Nuevos cambios agregads

--- proyecto.py
+++ proyecto.py
@@ -4,3 +4,5 @@ dolares = pesos / valor_dolar
 dolares = round(dolares, 2)
 dolares = str(dolares)
 print("Tienes \n $"+ dolares + " dolares" )
+
+menu = "Este es un nuevo texto, agrega tu otra parte compañero, gracias"
